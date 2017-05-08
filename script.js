2b1949d9e90adbb63c9c4db00f2b25e845246f3c
seunggabi <seunggabi.kim@navercorp.com>
2017-05-08 16:55:54 +0900

    fix ignore length

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
-function matching(target){
+// function matching(target){
+// 	chrome.tabs.executeScript({
+// 		code: 'document.querySelector("body").innerText'
+// 	}, function (result) {
+// 		document.querySelector('#target').value = target+"<br/>"+result;
+// 	});
+// }
+//
+//
+// chrome.storage.sync.get(function (data) {
+// 	document.querySelector('#target').value = data.target;
+// 	matching(data.target);
+// });
+//
+document.querySelector('#target').addEventListener('change', function () {
+	callback();
+});
+
+function callback() {
 	chrome.tabs.executeScript({
-		code: 'document.querySelector("body").innerText'
+		code: 'console.log(document.querySelector("body").innerText)'
 	}, function (result) {
-		console.log(target, result);
 	});
 }
-
-
-chrome.storage.sync.get(function (data) {
-	document.querySelector('#target').value = data.target;
-	matching(data.target);
-});
-
-document.querySelector('#target').addEventListener('change', function () {
-	var target = document.querySelector('#target').value;
-
-	chrome.storage.sync.set({
-		target: target
-	});
-
-	matching(target);
-});
