7abffaffe38f358d76ecfe67c394330b9a09bdf7
seunggabi <seunggabi.kim@navercorp.com>
2017-05-12 23:51:12 +0900

    add naver, youtube

--- script.js
+++ script.js
@@ -23,8 +23,24 @@
 // 	});
 // }
 
-function callback(event) {
-	console.log(event);
-};
+function callback() {
+	var youtube = document.querySelector(".videoAdUiSkipButton");
+	var naver = document.querySelector(".u_rmc_btn_skip");
 
-document.addEventListener('DOMSubtreeModified', callback);
+	youtube && (youtube.click() || console.log("YouTube"));
+	naver && (naver.click() || console.log("Naver"));
+}
+
+(function(){
+	var youtube = document.querySelector(".ad-container");
+	var naver = document.querySelector(".u_rmc_btn_skip");
+
+	var observer = new MutationObserver(function(mutations) {
+		mutations.forEach(function(mutationRecord) {
+			callback();
+		});
+	});
+
+	youtube && youtube.addEventListener('DOMSubtreeModified', callback);
+	naver && observer.observe(naver, { attributes : true, attributeFilter : ['style'] });
+})();
