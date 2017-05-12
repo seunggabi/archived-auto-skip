b313e157ade956be619eb9e9442e9b3e319323ba
seunggabi <seunggabi.kim@navercorp.com>
2017-05-13 00:19:47 +0900

    refactoring text, naver tv

--- script.js
+++ script.js
@@ -1,34 +1,9 @@
-// function matching(target){
-// 	chrome.tabs.executeScript({
-// 		code: 'document.querySelector("body").innerText'
-// 	}, function (result) {
-// 		document.querySelector('#target').value = target+"<br/>"+result;
-// 	});
-// }
-//
-//
-// chrome.storage.sync.get(function (data) {
-// 	document.querySelector('#target').value = data.target;
-// 	matching(data.target);
-// });
-//
-// document.querySelector('#target').addEventListener('change', function () {
-// 	callback();
-// });
-//
-// function callback() {
-// 	chrome.tabs.executeScript({
-// 		code: 'console.log(document.querySelector("body").innerText)'
-// 	}, function (result) {
-// 	});
-// }
-
 function callback() {
 	var youtube = document.querySelector(".videoAdUiSkipButton");
 	var naver = document.querySelector(".u_rmc_btn_skip");
 
 	youtube && (youtube.click() || console.log("YouTube"));
-	naver && (naver.click() || console.log("Naver"));
+	// naver && (naver.click() || console.log("Naver"));
 }
 
 (function(){
