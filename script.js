5d90d99bf098b5d1ddfa7b63697b6f630ba02171
seunggabi <seunggabi.kim@navercorp.com>
2017-05-15 14:46:42 +0900

    QA OK

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 function init() {
 	console.log("AutoSkip load!");
 
-	var youtube = document.querySelector(".ad-container");
+	var hostname = document.location.hostname;
+	var youtube = hostname === "www.youtube.com";
+	var twitch = hostname === "www.twitch.tv";
+	var jobplanet = hostname === "www.jobplanet.co.kr";
 	var naver = document.querySelector(".u_rmc_btn_skip");
-	var twitch = document.querySelector(".twitch-logo");
-	var jobplanet = document.querySelector("#video_ad");
 
 	var observer = new MutationObserver(function(mutations) {
 		mutations.forEach(function(mutationRecord) {
@@ -13,9 +14,9 @@ function init() {
 	});
 
 	youtube && (window.youtube = setInterval(callback, 300));
-	naver && observer.observe(naver, { attributes : true, attributeFilter : ["style"] });
 	twitch && (window.twitch = setInterval(callback, 300));
 	jobplanet && (window.jobplanet = setInterval(callback, 300));
+	naver && observer.observe(naver, { attributes : true, attributeFilter : ["style"] });
 }
 
 function callback() {
