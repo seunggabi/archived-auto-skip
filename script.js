61c41882fb3682a58ccd86821d566759fc391df8
seunggabi <seunggabi@naver.com>
2017-07-02 18:35:37 +0900

    add dailymotion, dramalink

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function init() {
 	var hostname = document.location.hostname;
 
-	var youtube = hostname === "www.youtube.com" || hostname === "www.dailymotion";
+	var youtube = hostname === "www.youtube.com";
 	var twitch = hostname === "www.twitch.tv";
 	var jobplanet = hostname === "www.jobplanet.co.kr";
 	var naver = hostname.indexOf("naver.com") != -1;
 	var kakao = hostname.indexOf("kakao.com") != -1;
 	var afreeca = hostname === "play.afreecatv.com";
 	var dramalink = hostname === "dramalink.net";
+	var dailymotion = hostname === "www.dailymotion.com";
 
 	youtube && (window.youtube = setInterval(callback, 300));
 	twitch && (window.twitch = setInterval(callback, 300));
@@ -15,7 +16,8 @@ function init() {
 	naver && (window.naver = setInterval(callback, 300));
 	kakao && (document.domain = "kakao.com") && (window.kakao = setInterval(callback, 300));
 	afreeca && (window.afreeca = setInterval(callback, 300));
-	dramalink && (window.dramalink = setInterval(callback, 300))
+	dramalink && (location.href = document.querySelector("iframe").src);
+	dailymotion && (window.dramalink = setInterval(callback, 300))
 }
 
 function callback() {
@@ -27,15 +29,15 @@ function callback() {
 	var kakao = document.querySelector("#player_iframe iframe") &&
 		document.querySelector("#player_iframe iframe").contentWindow.document.querySelector("#adSkipBtn");
 	var afreeca = document.querySelector("#promotion_btn_skip");
-	var dramalink = document.querySelector(".dmp_AdSkipButton-icon");
+	var dailymotion = document.querySelector(".dmp_AdSkipButton-icon");
 
-	youtube && (youtube.click() || console.log("YouTube"));
-	twitch && (clearInterval(window.twitch) || (twitch.src = "") || twitch.remove() || console.log("Twitch"));
-	jobplanet && (jobplanet.click() || console.log("JobPlanet"));
-	naver && naver.style.display !== "none" && (naver.click() || (naver.style.display = "none") && console.log("Naver"));
-	kakao && kakao.className.indexOf("hide") === -1 && (kakao.click() || (kakao.className += "hide") && console.log("Kakao"));
-	afreeca && afreeca.style.display !== "none" && afreeca.childElementCount === 1 && (afreeca.click() || console.log("Afreeca"));
-	dramalink && (dramalink.click() || console.log("DramaLink"));
+	youtube && youtube.click();
+	twitch && (clearInterval(window.twitch) || (twitch.src = "") || twitch.remove());
+	jobplanet && jobplanet.click();
+	naver && (naver.style.display !== "none" && naver.click() || (naver.style.display = "none"));
+	kakao && (kakao.className.indexOf("hide") === -1 && kakao.click() || (kakao.className += "hide"));
+	afreeca && (afreeca.style.display !== "none" && afreeca.childElementCount === 1 && afreeca.click());
+	dailymotion && dailymotion.click();
 }
 
 (function(){
