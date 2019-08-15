592a3bec1b4b5f590fc4bcbb60d4f813826c8f4d
seunggabi <seunggabi.kim@navercorp.com>
2019-08-15 23:39:19 +0900

    feat(#11): == -> ===

--- auto-skip.js
+++ auto-skip.js
@@ -54,10 +54,10 @@ function getCookie(cname) {
     var ca = decodedCookie.split(';');
     for(var i = 0; i <ca.length; i++) {
         var c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) === ' ') {
             c = c.substring(1);
         }
-        if (c.indexOf(name) == 0) {
+        if (c.indexOf(name) === 0) {
             return c.substring(name.length, c.length);
         }
     }
